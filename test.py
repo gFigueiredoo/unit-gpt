32acf22ed8e6c2ff3852eb319119624453f9143b
gabriel <gfigcontas@gmail.com>
2023-09-29 22:11:38 -0300

    push test to ut

---
+++ test.py
@@ -0,0 +1,11 @@
+def somar_dois_numeros(numero1, numero2):
+    resultado = numero1 + numero2
+    return resultado
+
+# Chamar a função e somar dois números
+numero1 = 5
+numero2 = 7
+
+soma = somar_dois_numeros(numero1, numero2)
+
+print(f"A soma de {numero1} e {numero2} é igual a {soma}")
